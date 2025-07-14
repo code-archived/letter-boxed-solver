7ef3fd2383f556d73d24f5f2085061c412587d74
ZenithClown <debmalyapramanik.005@gmail.com>
2025-07-14 21:12:22 +0530

    ✨ update module docstring and formatting

--- src/__init__.py
+++ src/__init__.py
@@ -2,5 +2,21 @@
 
 """
 Source Code to Solve the New York Times Letter Boxed Puzzle
+
+A Python Package to Solve the New York Times Letter Boxed Puzzle and
+intutively understand the problem. To understand the working principle
+check the readme file and example directory.
+
+@author: Debmalya Pramanik
+@copywright: 2024; Debmalya Pramanik (ZenithClown)
 """
 
+# ? package follows https://peps.python.org/pep-0440/
+# ? https://python-semver.readthedocs.io/en/latest/advanced/convert-pypi-to-semver.html
+__version__ = "v0.0.1.dev0"
+
+# init-time options registrations
+from src.corpus import WordCorpus
+from src.model import LetterBoxes
+
+from src.abstract import LetterBoxedModel
